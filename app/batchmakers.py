0f46b6f694b41b4fa0a094b26c87574f136ac2ab
MJ Rossetti <s2t2@users.noreply.github.com>
2024-04-02 12:04:51 -0400

    New batch-making function for dataframes

--- app/batchmakers.py
+++ app/batchmakers.py
@@ -40,3 +40,43 @@ def dynamic_batches(texts, batch_char_limit=30_000):
         batches.append(batch)
 
     return batches
+
+
+def dynamic_df_batches(df, text_colname="texts", batch_char_limit=30_000):
+    """Splits texts into batches, with specified max number of characters per batch.
+        Caps text length at the maximum batch size (individual text cannot exceed batch size).
+        Batches may have different lengths (in terms of number of rows).
+
+        Params df (has column of texts, as well as other columns like user_id we care about matching up with later)
+
+        Returns a list of one or more rows (pandas Series) per batch.
+    """
+    df = df.copy()
+    batches = []
+    batch = []
+    batch_chars = 0
+    for _, row in df.iterrows():
+        text = row[text_colname]
+        text_chars = len(text)
+        print(text)
+
+        if (batch_chars + text_chars) <= batch_char_limit:
+            # THERE IS ROOM TO ADD THIS TEXT TO THE BATCH
+            batch.append(row)
+            batch_chars += text_chars
+        else:
+            # NO ROOM IN THIS BATCH, START A NEW ONE:
+            if text_chars > batch_char_limit:
+                # CAP THE TEXT AT THE MAX BATCH LENGTH:
+                #text = text[0:batch_char_limit-1]
+                row[text_colname] = text[0:batch_char_limit-1]
+                text_chars = len(row[text_colname])
+            # CLEAR AND RESET THE BATCH:
+            batches.append(batch)
+            batch = [row]
+            batch_chars = text_chars
+
+    if batch:
+        batches.append(batch)
+
+    return batches
--- test/batchmakers_test.py
+++ test/batchmakers_test.py
@@ -1,19 +1,19 @@
 
 
 
-from app.batchmakers import split_into_batches, dynamic_batches
+from app.batchmakers import split_into_batches, dynamic_batches, dynamic_df_batches
 
+from pandas import DataFrame
 
-def test_batchmakers():
+texts = [
+    "Short and sweet",
+    "Short short",
+    "I like apples, but bananas are gross.",
+    "This is a tweet about bananas",
+    "Drink apple juice!",
+]
 
-    texts = [
-        "Short and sweet",
-        "Short short",
-        "I like apples, but bananas are gross.",
-        "This is a tweet about bananas",
-        "Drink apple juice!",
-    ]
-
+def test_split_into_batches():
     batches = list(split_into_batches(texts, batch_size=2))
     assert batches == [
         ['Short and sweet', 'Short short'], # 2
@@ -21,10 +21,29 @@ def test_batchmakers():
         ['Drink apple juice!'] # remainder
     ]
 
+def test_dynamic_batches():
     batches = dynamic_batches(texts, batch_char_limit=30)
     assert batches == [
         ['Short and sweet', 'Short short'],
         ['I like apples, but bananas ar'],
         ['This is a tweet about bananas'],
         ['Drink apple juice!']
- ]
+    ]
+
+def test_dynamic_df_batches():
+    texts_df = DataFrame({"text":texts, "user_id": [i for i in range(1, len(texts)+1)]})
+    batches = dynamic_df_batches(texts_df, text_colname="text", batch_char_limit=30)
+
+    records = []
+    for batch in batches:
+        batch_records = []
+        for row in batch:
+            batch_records.append(dict(row))
+        records.append(batch_records)
+
+    assert records == [
+        [{'text': 'Short and sweet', 'user_id': 1}, {'text': 'Short short', 'user_id': 2}],
+        [{'text': 'I like apples, but bananas ar', 'user_id': 3}],
+        [{'text': 'This is a tweet about bananas', 'user_id': 4}],
+        [{'text': 'Drink apple juice!', 'user_id': 5}]
+    ]
